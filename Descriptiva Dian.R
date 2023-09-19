59e57eb1ff569a2282376d9e7dad2a3fb62602c0
SofiaBello27 <abellod@unal.edu.co>
2023-09-19 04:51:40 -0500

    Cambios de Andres

--- Descriptiva Dian.R
+++ Descriptiva Dian.R
@@ -7,22 +7,30 @@ library(zoo)
 library(xts)
 library(readxl)
 library(tidyverse)
+### Transformacion de box cox ####
+library(forecast)
+MASS::boxcox(lm(dian2 ~ 1),seq(-5, 5, length = 50)) ##Notese que no acputra al 1
+forecast::BoxCox.lambda(dian2, method ="loglik",
+                        lower = -1, upper = 3)#Entrega el valor de lambda (0.1).
+plot(forecast::BoxCox(dian2,lambda=0.1))
+par(mar = c(1,1,1,1))
+ldian2=log(dian2)
+
+MASS::boxcox(lm(ldian2 ~ 1),seq(-5, 5, length = 50)) #Si captura al 1
+par(mfrow=c(2,1))
+plot(dian2,main="Serie Dian sin Transformar")
+plot(ldian2,main="Series Dian con Transformación BoxCox")
+
 #### Analisis descriptivo de la base de datos DIAN ######
-class(dian2)
+class(ldian2)
 
-# La serie es medida mensual, es decir, 
-# presenta una frecuencia de 12. 
-# Presenta  tendencia (aumenta), ciclos estacionales 
-# (cada año mas o menos), varianza marginal 
-# no constante (aumenta).
-
-dian3<-window(dian2, start = c(2000,1))
+dian3<-window(ldian2, start = c(2000,1))
 ts_plot(dian3,title="Serie de tiempo del recaudo mensual interno",
         Ytitle="Recaudo interno",
         Xtitle="Tiempo",
         Xgrid=TRUE,
         Ygrid=TRUE)
-### Estimación preliminar de la tendencia####
+### Estimación preliminar de la tendencia con objeto tibble (gráfico más bonito)####
 ##Algunos paquetes 
 require(feasts)
 require(fable)
@@ -50,12 +58,12 @@ tbl_dian%>%plot_time_series(.value=impuesto,.date_var=fecha)#gráfica timetk
 
 
 ### Analisis de tendencia con regresion simple ####
-summary(fit<-lm(dian2~time(dian2),na.action=NULL))
-plot(dian2,ylab="Recaudo interno") 
+summary(fit<-lm(ldian2~time(ldian2),na.action=NULL))
+plot(ldian2,ylab="Recaudo interno") 
 abline(fit,col="darkcyan",lwd=2)
 #Eliminamos la tendencia con la predicción de la recta
-ElimiTenddian<-dian2-predict(fit)
-plot(ElimiTenddian,main="Serie Dian sin tendencia",
+ElimiTenddian<-ldian2-predict(fit)
+plot(ElimiTenddian,main="Serie Dian sin tendencia y con varianza estable",
      cex.main=1.3,
      xlab="Tiempo",
      ylab="Recaudo interno",
@@ -64,16 +72,16 @@ acf(ElimiTenddian,lag.max=179)
 
 ###Loess para hacer el ajuste de la tendencia ####
 
-### Descomposición ####
-dian_decompo=decompose(dian2)
+### Descomposición con filtro de promedios moviles ####
+dian_decompo=decompose(ldian2)
 plot(dian_decompo)
 dian_decompo$trend
-### DEscomposición STL ####
+### Descomposición STL ####
 ##Algunas librerias
 library(feasts)
 library(fable)
 ### Gráfico ##
-tsibble_dian<-as_tsibble(dian2)
+tsibble_dian<-as_tsibble(ldian2)
 str(tsibble_dian)
 tsibble_dian %>%
   model(
@@ -82,69 +90,67 @@ tsibble_dian %>%
         robust = TRUE)) %>%
   components() %>%
   autoplot()
+tsibble_dian_notendstl<-tsibble_dian$Log
+##
+## Aqui podriamos colocar lo que sale en el archivo ejemplodescriptiva.qmd
+## Para eliminar la tendencia con este metodo.
+##
 
-
-### Aun no se que es esto pero quita tendencia xd ####
+### Aun no se que es esto pero quita tendencia (creo que hace lo mismo que al quitarla con lm) xd ####
 par(mar = c(2,2,2,2))
-fitdian = lm(dian2~time(dian2), na.action=NULL) # Regresión sobre el tiempo
+fitdian = lm(ldian2~time(ldian2), na.action=NULL) # Regresión sobre el tiempo igual que el objeto 
 par(mfrow=c(2,1))
 plot(resid(fitdian), type="l", main="sin tendencia") #Sin tendencia quitandola con regresión
 plot(diff(dian2), type="l", main="Primera Diferencia") #Primera diferencia ordinaria
 ## Gráfico de los acf
 par(mar = c(3,2,3,2))
 par(mfrow=c(3,1)) # plot ACFs
-acf(dian2, 60, main="ACF Dian")
-acf(resid(fitdian), 60, main="ACF Sin tendencia") 
+acf(ldian2, 60, main="ACF Dian objeto ts varianza estable por boxcox")
+acf(resid(fitdian), 60, main="ACF Sin tendencia (resid(fitdian))") 
 acf(diff(dian2), 60, main="ACF Primera Diferencia")
 #Notese que no bajan tan rapido sino que lo hacen lentico 
 #y tiene un ciclo estacional
-### Transformacion de box cox ####
-library(forecast)
-forecast::BoxCox.lambda(dian2, method ="loglik",
-                        lower = -1, upper = 3)#Entrega el valor de lambda (0.1).
-plot(forecast::BoxCox(dian2,lambda=0.1))
-par(mar = c(1,1,1,1))
-par(mfrow=c(2,1))
-ldian2=log(dian2)
-plot(dian2,main="Serie Dian sin Transformar")
-plot(ldian2,main="Series con Transformación BoxCox")
-
-## Gráfica de retardos #####
-ts_info(dian2)
+## Gráfica de retardos con varianza estable #####
+ts_info(ldian2)
 par(mar = c(3,2,3,2))
-astsa::lag1.plot(dian2, 12,corr=F)
-
-ts_lags(soi,lags=1:12)
-## Preguntar si estos graficos se hacen sin tendencia.
+astsa::lag1.plot(ldian2, 12,corr=F)
+ts_lags(ldian2,lags=1:12)
+# Grafica retardos sin tendencia y varianza estable
+ts_info(ElimiTenddian)
+par(mar = c(3,2,3,2))
+astsa::lag1.plot(ElimiTenddian, 12,corr=F)
+ts_lags(ElimiTenddian,lags=1:12)
+#### Gráfico  de perfiles 
+ggseasonplot(ElimiTenddian)
 ##ACF y PACF #####
 par(mfrow=c(3,1))
 par(mar = c(2.7,2,2.7,2))
-acf(ElimiTenddian, 48, main="Impuestos Dian tendencia eliminada con lm")
-acf(resid(fitdian), 48, main="Impuestos Dian la otra forma de quitar tendencia")
-acf(diff(dian2),48,main="Impuestos Dian diferenciación")
+acf(ElimiTenddian, 48, main="Impuestos Dian tendencia eliminada con lm y varianza estable")
+acf(resid(fitdian), 48, main="Impuestos Dian la otra forma de quitar tendencia y varianza estable")
+acf(diff(ldian2),48,main="Impuestos Dian diferenciación variazna estable")
 
 ## Indice AMI 
 library(nonlinearTseries)
 library(tseriesChaos)
-tseriesChaos::mutual(dian2, partitions = 50, lag.max = 10, plot=TRUE)
-tseriesChaos::mutual(ElimiTenddian, partitions = 50, lag.max = 10, plot=TRUE)
+tseriesChaos::mutual(ldian2, partitions = 50, lag.max = 10, plot=TRUE,main="AMI con tendencia y varianza estable")
+tseriesChaos::mutual(ElimiTenddian, partitions = 50, lag.max = 10, plot=TRUE,main="AMI sin tendencia y varianza estable ")
 
 ## Detección de cíclos y estacionalidades ####
-TSstudio::ts_heatmap(dian2,title = "Mapa de Calor - Impuestos Dian con tendencia")
+TSstudio::ts_heatmap(ldian2,title = "Mapa de Calor - Impuestos Dian con tendencia")
 TSstudio::ts_heatmap(ElimiTenddian,title = "Mapa de Calor - Impuestos Dian sin tendencia")
-TSstudio::ts_heatmap(diff(dian2),title = "Mapa de Calor - Impuestos Dian sin tendencia")
+TSstudio::ts_heatmap(diff(ldian2),title = "Mapa de Calor - Impuestos Dian sin tendencia")
 
 ## Otras medidas para observar la estacionalidad.
-## Con la serie con tendencia 
+## Con la serie con tendencia y varianza estable
 library(dplyr)
 library(plotly)
-dian2_df <- data.frame(year = floor(time(dian2)), month = cycle(dian2),dian2 = as.numeric(dian2))
+dian2_df <- data.frame(year = floor(time(ldian2)), month = cycle(ldian2),ldian2 = as.numeric(ldian2))
 dian2_df$month <- factor(month.abb[dian2_df$month], levels = month.abb)
-dian2_summary <- dian2_df %>%group_by(month) %>%summarise(mean= mean(dian2),sd = sd(dian2))
+dian2_summary <- dian2_df %>%group_by(month) %>%summarise(mean= mean(ldian2),sd = sd(ldian2))
 dian2_summary
 plot_ly (data = dian2_summary, x = ~ month, y = ~ mean, type = "bar", name   = "Mean") %>%
-  layout (title = "dian2 - Monthly Average", yaxis =list(title = "Mean",   range = c(4136400, 9148475)))
-## Falta hacer el mismo analisis anterior pero sin tendencia ####
+  layout (title = "ldian2 - Monthly Average", yaxis =list(title = "Mean",   range = c(15, 16)))
+## Con la serie sin tendencia eliminada por lm y con la varianza estable ####
 dian2sint_df <- data.frame(year = floor(time(ElimiTenddian)), month = cycle(ElimiTenddian),ElimiTenddian = as.numeric(ElimiTenddian))
 dian2sint_df$month <- factor(month.abb[dian2sint_df$month], levels = month.abb)
 dian2sint_summary <- dian2sint_df %>%group_by(month) %>%summarise(mean= mean(ElimiTenddian),sd = sd(ElimiTenddian))
@@ -152,20 +158,22 @@ dian2sint_summary
 plot_ly (data = dian2sint_summary, x = ~ month, y = ~ mean, type = "bar", name   = "Mean") %>%
   layout (title = "dian2sint - Monthly Average", yaxis =list(title = "Mean",   range = c(min(dian2sint_summary$mean), max(dian2sint_summary$mean))))
 
-## Gráfico de subseries ####
-dian2_tsbl=as_tsibble(dian2)
+## Gráfico de subseries con varianza estabilizada ####
 require(feasts)
-dian2_tsbl=as_tsibble(dian2)
+dian2_tsbl=as_tsibble(ldian2)
 dian2_tsbl%>%gg_subseries(value)
-
+## Gráfico de subseries sin tendencia y con varianza estabilizada
+require(feasts)
+dian2_tsbl_notend=as_tsibble(ElimiTenddian)
+dian2_tsbl_notend%>%gg_subseries(value)
 ## Usando regresión para descubrir un ciclo (periodograma) ####
 ##Periodograma 
-#con tendencia 
-spectrum(dian2,log='no')
+#con tendencia y varianza estabilizada
+spectrum(ldian2,log='no')
 abline(v=1/12, lty=2,col="red")
-spectrum(dian2,log='no',span=5)
-spectrum(dian2,log='no',span=c(5,5))
-spectrum(dian2,log='no',span=c(2,2))
+spectrum(ldian2,log='no',span=5)
+spectrum(ldian2,log='no',span=c(5,5))
+spectrum(ldian2,log='no',span=c(2,2))
 #Sin tendencia 
 spectrum(ElimiTenddian,log='no')
 abline(v=1/12, lty=2,col="red")
@@ -173,3 +181,9 @@ spectrum(ElimiTenddian,log='no',span=5)
 spectrum(ElimiTenddian,log='no',span=c(5,5))
 spectrum(ElimiTenddian,log='no',span=c(2,2))
 
+Periodgramadldian2_sintendencia=spectrum(as.numeric(ElimiTenddian),log='no')
+ubicacionlogdian=which.max(Periodgramadldian2_sintendencia$spec)
+sprintf("El valor de la frecuencia donde se máximiza el periodograma para la serie es: %s",Periodgramadldian2_sintendencia$freq[ubicacionlogdian])
+
+sprintf("El periodo correspondiente es aproximadamente: %s",1/Periodgramadldian2_sintendencia$freq[ubicacionlogdian])
+### Modelamiento de la estacionalidad #####
--- Descriptiva Energia.R
+++ Descriptiva Energia.R
@@ -7,16 +7,20 @@ library(zoo)
 library(xts)
 library(readxl)
 library(tidyverse)
-#### Analisis descriptivo de la base de datos DIAN ######
-class(energia2)
-
-# La serie es medida mensual, es decir, 
-# presenta una frecuencia de 12. 
-# Presenta  tendencia (aumenta), ciclos estacionales 
-# (cada año mas o menos), varianza marginal 
-# no constante (aumenta).
-
-energia3<-window(energia2, start = c(2004,10))
+### Transformacion de box cox con objeto ts####
+MASS::boxcox(lm(energia2 ~ 1),seq(-5, 5, length = 50)) ##Notese que no acputra al 1
+forecast::BoxCox.lambda(energia2, method ="loglik",
+                        lower = -1, upper = 3)#Entrega el valor de lambda (0.1).
+plot(forecast::BoxCox(energia2,lambda=0.35))
+par(mar = c(1,1,1,1))
+lenergia2=log(energia2)
+MASS::boxcox(lm(lenergia2 ~ 1),seq(-5, 5, length = 50))#Si captura al 1 auqnue por pqouito, toca preguntarle al profe que hacer ahí 
+abline(v = 1, col = "red", lty = 2)
+par(mfrow=c(2,1))
+plot(energia2,main="Serie energia sin Transformar")
+plot(lenergia2,main="Series energia con Transformación BoxCox")
+#### Analisis descriptivo de la base de datos energia ######
+energia3<-window(lenergia2, start = c(2004,10))
 ts_plot(energia3,title="Serie de tiempo del recaudo mensual interno",
         Ytitle="Recaudo interno",
         Xtitle="Tiempo",
@@ -38,7 +42,7 @@ energia_xts=xts(x = energia_1$Energia,frequency = 365.25,order.by = Fechas)
 ts_info(energia_xts)
 plot(energia_xts)
 
-###Creación objeto tssible a partir de un objeto tibble
+###Creación objeto tssible a partir de un objeto tibble#####
 df_energia=data.frame(Energia=energia_1$Energia,fecha=energia_1$fecha)
 tbl_energia=tibble(df_energia)
 tbl_energia_format_fecha=tbl_energia
@@ -51,11 +55,11 @@ tbl_energia%>%plot_time_series(.value=Energia,.date_var=fecha)#gráfica timetk
 
 
 ### Analisis de tendencia con regresion simple ####
-summary(fit_e<-lm(energia2~time(energia2),na.action=NULL))
-plot(energia2,ylab="Consumo de energia total.") 
+summary(fit_e<-lm(lenergia2~time(lenergia2),na.action=NULL))
+plot(lenergia2,ylab="Consumo de energia total.") 
 abline(fit_e,col="darkcyan",lwd=2)
 #Eliminamos la tendencia con la predicción la recta
-ElimiTendenerg<-energia2-predict(fit_e)
+ElimiTendenerg<-lenergia2-predict(fit_e)
 plot(ElimiTendenerg,main="Serie energia sin tendencia",
      cex.main=1.3,
      xlab="Tiempo",
@@ -65,16 +69,19 @@ acf(ElimiTendenerg,lag.max=179)
 
 ###Loess para hacer el ajuste de la tendencia ####
 
-### Descomposición ####
-energia_decompo=decompose(energia2)
+### Descomposición filtro de promedio moviles####
+energia_decompo=decompose(lenergia2)
 plot(energia_decompo)
 energia_decompo$trend
-### DEscomposición STL ####
+### Descomposición STL (no está funcionando)####
 ##Algunas librerias
 library(feasts)
 library(fable)
 ### Gráfico ##
-tsibble_energia<-as_tsibble(energia2)
+
+tsibble_energia<-as_tsibble(lenergia2)
+tsibble_energia <- tsibble_energia %>%
+  fill_gaps()
 str(tsibble_energia)
 tsibble_energia %>%
   model(
@@ -85,36 +92,28 @@ tsibble_energia %>%
   autoplot()
 ### Aun no se que es esto pero quita tendencia xd ####
 par(mar = c(2,2,2,2))
-fit_e = lm(energia2~time(energia2), na.action=NULL) # Regresión sobre el tiempo
+fit_e = lm(lenergia2~time(lenergia2), na.action=NULL) # Regresión sobre el tiempo
 par(mfrow=c(2,1))
 plot(resid(fit_e), type="l", main="sin tendencia") #Sin tendencia quitandola con regresión
-plot(diff(energia2), type="l", main="Primera Diferencia") #Primera diferencia ordinaria
+plot(diff(lenergia2), type="l", main="Primera Diferencia") #Primera diferencia ordinaria
 ## Gráfico de los acf
 par(mar = c(3,2,3,2))
 par(mfrow=c(3,1)) # plot ACFs
-acf(energia2, 60, main="ACF energia")
+acf(lenergia2, 60, main="ACF energia")
 acf(resid(fit_e), 60, main="ACF Sin tendencia") 
-acf(diff(energia2), 60, main="ACF Primera Diferencia")
+acf(diff(lenergia2), 60, main="ACF Primera Diferencia")
 #Notese que no bajan tan rapido sino que lo hacen lentico 
 #y tiene un ciclo estacional
-### Transformacion de box cox ####
-library(forecast)
-forecast::BoxCox.lambda(energia2, method ="loglik",
-                        lower = -1, upper = 3)#Entrega el valor de lambda (0.1).
-plot(forecast::BoxCox(energia2,lambda=0.1))
-par(mar = c(1,1,1,1))
-par(mfrow=c(2,1))
-
-lenergia2=log(energia2)
-plot(energia2,main="Serie de energia sin transformar")
-plot(lenergia2,main="Series con Transformación BoxCox")
-
 ## Gráfica de retardos #####
-ts_info(energia2)
+ts_info(lenergia2)
 par(mar = c(3, 2, 3, 2))
-astsa::lag1.plot(energia2, 7,corr=F)
+astsa::lag1.plot(lenergia2, 7,corr=F)
 
-ts_lags(soi,lags=1:7)
+ts_info(ElimiTendenerg)
+par(mar = c(3, 2, 3, 2))
+astsa::lag1.plot(ElimiTendenerg, 7,corr=F)
+
+ts_lags(ElimiTendenerg,lags=1:7)
 ## Preguntarle al profe cual es el valor que debe ir en la funcion astsa
 ## Preguntar si estos graficos se hacen sin tendencia.
 ##ACF y PACF #####
@@ -122,20 +121,18 @@ par(mfrow=c(3,1))
 par(mar = c(2.7,2,2.7,2))
 acf(ElimiTendenerg, 48, main="Energia tendencia eliminada con lm")
 acf(resid(fit_e ), 48, main="Energia la otra forma de quitar tendencia")
-acf(diff(energia2),48,main="Energia diferenciación")
+acf(diff(lenergia2),48,main="Energia diferenciación")
 ## Indice AMI 
 library(nonlinearTseries)
 library(tseriesChaos)
-tseriesChaos::mutual(energia2, partitions = 50, lag.max = 10, plot=TRUE)
+tseriesChaos::mutual(lenergia2, partitions = 50, lag.max = 10, plot=TRUE)
 tseriesChaos::mutual(ElimiTendenerg, partitions = 50, lag.max = 10, plot=TRUE)
 
-## Detección de cíclos y estacionalidades ####
-energia3<-ts(energia$Energia,start=c(2004,10,01),frequency=365)
-TSstudio::ts_heatmap(energia3,title = "Mapa de Calor - Impuestos Dian con tendencia")
+## Detección de cíclos y estacionalidades (no está funcionando) ####
+energia3<-ts(energia$Energia,start=c(2004,10,01),frequency=365.25)
+TSstudio::ts_heatmap(energia3,title = "Mapa de Calor - energia con tendencia")
 #TSstudio::ts_heatmap(ElimiTendenerg,title = "Mapa de Calor - Impuestos Dian sin tendencia")#Lo dejo comentado porque aun me falta quitar la tendencia de este modo para energia3
-TSstudio::ts_heatmap(diff(energia3),title = "Mapa de Calor - Impuestos Dian sin tendencia")
-##Creo que no se ve nada, preguntar al profe.
-
+TSstudio::ts_heatmap(diff(energia3),title = "Mapa de Calor - energia sin tendencia")
 ## Explorando multiples estacionalidades ######
 library(UKgrid)
 require(TSstudio)
@@ -145,18 +142,30 @@ require(tsibble)
 require(plotly)
 
 view(energia2)
+energia_df <- data.frame(time = zoo::index(lenergia2), energia=as.numeric(lenergia2))
+str(energia_df)
 
+energia_df$hour <- hour(energia$fecha)
+energia_df$weekday <- wday(energia_df$fecha, label = TRUE, abbr = TRUE)
+UKgrid_df$month <- factor(month.abb[month(UKgrid_df$time)], levels =   month.abb)
+head(UKgrid_df)
 ## Usando regresión para descubrir un ciclo (periodograma) ####
 ##Periodograma 
 #con tendencia 
-spectrum(energia2,log='no')
+spectrum(lenergia2,log='no')
 abline(v=1/365, lty=2,col="red")
-spectrum(energia2,log='no',span=5)
-spectrum(energia2,log='no',span=c(5,5))
-spectrum(energia2,log='no',span=c(2,2))
+spectrum(lenergia2,log='no',span=5)
+spectrum(lenergia2,log='no',span=c(5,5))
+spectrum(lenergia2,log='no',span=c(2,2))
 #Sin tendencia 
 spectrum(ElimiTendenerg,log='no')
-abline(v=1/12, lty=2,col="red")
+abline(v=1/365, lty=2,col="red")
 spectrum(ElimiTendenerg,log='no',span=5)
 spectrum(ElimiTendenerg,log='no',span=c(5,5))
 spectrum(ElimiTendenerg,log='no',span=c(2,2))
+
+Periodgramadlenergia2_sintendencia=spectrum(as.numeric(ElimiTendenerg),log='no')
+ubicacionlogenergia=which.max(Periodgramadlenergia2_sintendencia$spec)
+sprintf("El valor de la frecuencia donde se máximiza el periodograma para la serie es: %s",Periodgramadlenergia2_sintendencia$freq[ubicacionlogenergia])
+
+sprintf("El periodo correspondiente es aproximadamente: %s",1/Periodgramadlenergia2_sintendencia$freq[ubicacionlogenergia])
