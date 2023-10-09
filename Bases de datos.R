e7f97cfc79e6f9942297c3b1a8d45c3b7b76cbf3
SofiaBello27 <abellod@unal.edu.co>
2023-10-09 09:15:48 -0500

    enrgia.csv

--- Bases de datos.R
+++ Bases de datos.R
@@ -35,12 +35,15 @@ plot(dian2, main="Serie de tiempo del recaudo mensual interno",
 #### Base de datos de energia ####
 AEP_hourly<-read.csv("AEP_hourly.csv")
 AEP_hourly$Datetime<-as.POSIXct(AEP_hourly$Datetime, format = "%Y-%m-%d %H:%M:%S")
+AEP_hourly<-AEP_hourly %>% arrange(Datetime)
 AEP_hourly$fecha<-as.Date(AEP_hourly$Datetime)
 
 energia <- AEP_hourly %>%
   group_by(fecha) %>%
   summarise(Energia = sum(AEP_MW))
 energia<-energia[-5055,]
+
+write_csv(energia, "energia.csv")
 
 ## Serie de tiempo de la energia ##
 energia2<-ts(energia$Energia,start=c(2004,10,01),frequency=365.25)
@@ -51,10 +54,3 @@ plot(energia2, main="Serie de tiempo de la energía diaria de una empresa estado
      cex.lab=0.4)
 
 plot(as.ts(energia$Energia))
-
-
-
-
-
-
-
